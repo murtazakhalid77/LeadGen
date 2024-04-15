ae9204f7431a7bf0c9158668885d3f07c0ba39c1
Murtaza <Murtaza>
2024-04-15 17:10:10 +0500

    prod

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -56,7 +56,7 @@ final Map<String, WidgetBuilder> routes = {
 };
 
 class UrlConfig {
-static const String baseUrl = "http://192.168.1.104:8080/api";  
+static const String baseUrl = "https://api.leadgen.stepwaysoftwares.com/api";
 
 
   static Uri buildUri(String path) {
